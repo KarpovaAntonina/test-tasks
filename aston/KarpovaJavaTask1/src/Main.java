25f0452673882fa5d18c3c48b4b052a0096ea5e5
KarpovaAntonina <karpova.antonina.u@yandex.ru>
2024-09-21 23:43:50 +0700

    задание 1 готово

--- aston/KarpovaJavaTask1/src/Main.java
+++ aston/KarpovaJavaTask1/src/Main.java
@@ -1,15 +1,43 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import static java.lang.System.exit;
+
+/*
+   1. Написать программу, которая принимает на вход два целых числа (a и b) и совершает с ними следующие действия:
+   - сравнивает эти два числа и возвращает результат сравнения путем вывода в консоль одного из вариантов: "a > b", "a < b" или "a = b";
+   - совершает с этими числами операции сложения, вычитания, деления и умножения и результат выводит в консоль.
+    */
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        if (args.length != 2) {
+            System.out.println("wait two params");
+            exit(1);
+        }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+
+        if (a == b) {
+            System.out.println("a==b");
+        } else if (a < b) {
+            System.out.println("a<b");
+        } else {
+            System.out.println("a>b");
+        }
+
+        System.out.print("a + b = ");
+        System.out.println(a + b);
+
+        System.out.print("a - b = ");
+        System.out.println(a - b);
+
+        System.out.print("a * b = ");
+        System.out.println(a * b);
+
+        if (b != 0) {
+            System.out.print("a / b = ");
+            System.out.println(1.0 * a / b);
+        } else {
+            System.out.println("b = 0. Делить нельзя");
         }
     }
 }
+
