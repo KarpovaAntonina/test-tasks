aa410f147148076c1c961bf1cd71fa800ea330b2
KarpovaAntonina <karpova.antonina.u@yandex.ru>
2024-09-21 22:42:09 +0700

    Задание 3 готово

--- aston/KarpovaJavaTask3/src/Main.java
+++ aston/KarpovaJavaTask3/src/Main.java
@@ -1,15 +1,15 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/*
+3. Задан массив целых чисел: [1, 2, 3, 4, 5, 6, 7, 8, 9,10] необходимо написать программу,
+которая выведет в консоль все чётные числа.
+*/
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                System.out.println(array[i]);
+            }
         }
     }
 }
