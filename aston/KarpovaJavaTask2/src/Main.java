b12a1855955c2b8d0a8f2c468c61f7d98c2341f6
KarpovaAntonina <karpova.antonina.u@yandex.ru>
2024-09-22 00:04:25 +0700

    задание 2 готово

--- aston/KarpovaJavaTask2/src/Main.java
+++ aston/KarpovaJavaTask2/src/Main.java
@@ -1,15 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import static java.lang.System.exit;
+
+/*
+2. Написать программу, которая принимает на вход две строки (a и b) и сравнивает их.
+В результате сравнения в консоль должно быть выведено одно из сообщений: "Строки неидентичны" или "Строки идентичны"
+*/
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        if (args.length != 2) {
+            System.out.println("wait two params");
+            exit(1);
+        }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        String a = (args[0]);
+        String b = (args[1]);
+
+        if (a.equals(b)) {
+            System.out.println("Строки идентичны");
+        } else {
+            System.out.println("Строки неидентичны");
         }
     }
 }
